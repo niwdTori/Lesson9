6b3ee3bb38f371f0640d1cb7b424e54529d99aee
Tori <mes.viktoria@mail.ru>
2022-09-20 00:59:47 +0500

    Task 64

---
+++ Task064/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void GetNumbers(int num1, int num2)
+{
+    int max = num1;
+    int min = num2;
+    if (max < num2)    
+    {
+        max = num2;
+        min = num1;
+    }
+    while (min < max)
+    {
+        Console.Write($"{min}, ");
+        min++;
+    }
+    if(min==max) Console.Write(max);
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+GetNumbers(m, n);
