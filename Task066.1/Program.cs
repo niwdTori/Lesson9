6d691e4c7b9d7fb0c7c68f7864dd3d2da5c86cab
Tori <mes.viktoria@mail.ru>
2022-09-21 20:57:00 +0500

    Task 66.Correction

---
+++ Task066.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSumNumbers(int num1, int num2)
+{
+    int sum = num1;
+    if (num1==num2) return sum;
+    sum += GetSumNumbers(num1 + 1, num2);
+    return sum;
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+int result = GetSumNumbers(m, n);
+Console.WriteLine($"Сумма элементов: {result}");
